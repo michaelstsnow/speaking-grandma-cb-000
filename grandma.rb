7aa9dfd4b5c3691526a52612b8d1a6c3e84268a7
michaelstsnow <michael.st.snow@gmail.com>
2018-10-19 18:39:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,14 +14,17 @@
 def speak_to_grandma(phrase)
 
   if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-    return "NO, NOT SINCE 1938!"
+    #puts "NO, NOT SINCE 1938!"
+    #return "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    #puts "I LOVE YOU TOO PUMPKIN!"
+    #return "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
-    return "HUH?! SPEAK UP, SONNY!"
+    #puts "HUH?! SPEAK UP, SONNY!"
+    #return "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 
 end
